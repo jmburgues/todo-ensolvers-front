726ffdbc8b192d08f840c1d61f528cf6fcfd5775
Juan Manuel Burgues Schaab <securetux@gmail.com>
2021-04-29 15:12:45 -0300

    Adds headers, footers and Css style sheet

---
+++ Views/header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+     <!--CSS-->
+     <link rel="stylesheet" href="<?php echo FRONT_ROOT ?>Views/css/style.css">
+
+     <title>ToDo - Ensolvers Challenge</title>
+
+</head>
+<body>
